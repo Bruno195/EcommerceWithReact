d060ceabd2f14b8a71c7716e7d53647b873e4986
Bruno195 <brunomonteiro195@gmail.com>
2021-12-23 21:28:12 -0200

    Alterações de projeto

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,27 @@
+import React from "react";
+import Header from "./components/Header"
+import Footer from './components/Footer'
+import HomeScreen from "../src/components/screens/HomeScreen"
+import { Container } from "react-bootstrap"
+import "./app.css"
+
 
 
 const App = () => {
   return (
-    <div>
-      <h1>Welcome to ProShop</h1>
+    <>
+      <Header />
+      <main className="py-3">
 
-    </div>
+        <Container>
+          <HomeScreen />
+        </Container>
+
+
+      </main>
+      <Footer />
+
+    </>
   );
 }
 
